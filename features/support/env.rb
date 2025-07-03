aa7673bbd65331a4c0e6fa95369c25c5af46924f
dendyp <dendy@machtwatch.co.id>
2025-07-03 14:16:50 +0700

    setup framework

---
+++ features/support/env.rb
@@ -0,0 +1,16 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'cucumber'
+require 'rspec'
+require 'pry'
+require 'webdrivers'
+require 'dotenv/load'
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30)
+end
+
+Capybara.configure do |config| 
+  config.default_driver = :chrome
+  config.default_max_wait_time = 30
+end
